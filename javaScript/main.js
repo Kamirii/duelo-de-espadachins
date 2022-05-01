8d8298c749a86bc272dc7127136622c5a3cb0dc8
Kamirii <camilyalbres@gmail.com>
2022-05-01 02:22:36 -0300

    first commit

---
+++ javaScript/main.js
@@ -0,0 +1,15 @@
+var mizuTogugawa = 1
+var haikyuuTatakae = 2
+var otaeMizuya = 3
+var personagem = [mizuTogugawa,haikyuuTatakae,otaeMizuya]
+mizuTogugawa = "Mizu Togugawa"
+haikyuuTatakae= "Haikyuu Tatakae"
+otaeMizuya = "Otae Mizuya"
+
+//  while(personagem1 != 1 && personagem2 != 2 && personagem3 != 3 ){
+//  var input = prompt("Escolha um dos três espadachins:")
+
+    
+// }
+console.log(personagem)
+function change(nome, sobrenome)
