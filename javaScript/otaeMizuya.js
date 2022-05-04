0cf9d99d0ebad84763a65564684007e5aa863565
Kamirii <camilyalbres@gmail.com>
2022-05-04 12:36:32 -0300

    otae jogavel

--- javaScript/otaeMizuya.js
+++ javaScript/otaeMizuya.js
@@ -1,26 +1,29 @@
 let sim = 1
 let nao = 2
 
-alert("Tudo ok?")
-
 function tratamento(){
     entrada = prompt("Digite o número das opções")
-    while(entrada != 1 || entrada != 2 || entrada !=0){ 
+    while(entrada != 1 || entrada != 2 || entrada != 3 ||  entrada != 4 || entrada !=0){ 
 
-        if(entrada == sim || entrada == nao){
-            return faseTatakae()
+        if(entrada == 1 || entrada == 2  || entrada == 3|| entrada == 4){
+            return faseMizuya()
         }else{
-            alert("Digite apenas o número das opções sim ou não")
+            alert("Digite apenas os números indicados")
             return tratamento()
         }
     }
 }
 
-function faseTatakae(){
-    if(entrada == sim){
+function faseMizuya(){
+    if(entrada == 1){
         return location = "fase2Otae.html"
     }else if(entrada == 2){
         return location = "gameOverOtae.html"
+    } if(entrada == 3){
+        return location = "fase3Otae.html"
     }
-}
-//colocar contador
+     if(entrada == 4){
+        return location = "fase4OTae.html"
+    }
+    
+}   
